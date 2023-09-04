5be88d452d366fd015e57e4bfc0fcd923c470159
LuanaMorais2 <luana.morais2@outlook.com>
2023-09-04 17:41:59 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/lumorais/hruser/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.lumorais.hruser.repositories;
+
+import com.lumorais.hruser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
